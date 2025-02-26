311f90916d463ea9b5c0acbc3db00f711544abd8
AldoReynoso09 <up240238@alumnos.upa.edu.mx>
2025-02-26 14:57:17 -0600

    01_Dayexercise

---
+++ 01_Dayexercise.py
@@ -0,0 +1,22 @@
+#2
+print(8+6)
+print(8-15)
+print(5*12)
+print(8/2)
+print(5**3)
+print(6%3)
+print(48//12)
+#3
+print('Aldo')
+print('Reynoso Gonalez')
+print('México')
+print('I am enjoyong 30 days of paython')
+#4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type['Asanbeneh,Python,Finland'])
+print(type('Aldo'))
+print(type('Reynoso Gonalez'))
+print(type('México'))
