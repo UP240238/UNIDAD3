1e8767df302e99d5ae5d447425482948c29d2cf6
AldoReynoso09 <up240238@alumnos.upa.edu.mx>
2025-03-03 12:07:35 -0600

    03_Dayexercise

--- 03_Dayexercise.py
+++ 03_Dayexercise.py
@@ -38,9 +38,12 @@ pi=int (3.1416)
 circunference= (2*pi*radio)
 print('The circunference of the circle is: ', circunference)
 #Número 8 
-x=input('Enter x')
-y=input('Enter y')
-slope= ()
+slope = 2  
+y_intercept = -2  
+x_intercept = -y_intercept / slope  
+print(f"Pendiente: {slope}")
+print(f"Intersección con el eje y: (0, {y_intercept})")
+print(f"Intersección con el eje x: ({x_intercept}, 0)")
 #Número9
 x1 = float(input('Enter x1:'))
 y1 = float(input('Enter y1:'))
@@ -53,8 +56,53 @@ print('The Euclidean distance is:', distancia)
 
 #Número 10
 #Número 11 
+x = -3 
+y = x**2 + 6*x + 9  
+es_cero = (y == 0)  
+es_negativo = (x < 0)  
+es_no_positivo = not (y > 0) 
+print("y es 0:", es_cero and "Sí" or "No")
+print("x es negativo:", es_negativo and "Sí" or "No")
+print("y NO es positivo:", es_no_positivo and "Sí" or "No")
+#Número 12
+python = len('python')
+dragon = len('dragon')
+falsy_comparison = python > dragon and python < dragon  
+#Número 13 
+on='on'in 'python'and 'on' in 'dragon'
+#Número 14 
+texto='I hope this course is not full of jargon'
+jargon= 'jargon'in texto 
+#Número 15 
+on=not ('on'in 'python'and 'on' in 'dragon')
+#Número 16
+python = len('python')
+python_float=float(python)
+python_str=str(python_float)
+#Número 17 
+num=int(input('Ingresa el número'))
+print(num % 2 == 0)
+#Número 18 
+division_floor=(7//3)==int(2.7)
 #Número19
 num = input('num:')
 if (num==10):
     print('The num is equal 10 ')
 else: "The num is different to 10"
+#Número 20
+int=int(float('9.8'))==10 
+#Número 21 
+horas=float(input('Ingresa las horas '))
+tarifa=float (input('Ingresa la trafia por hora'))
+pago= horas* tarifa
+print('Tu pago semanal es ', pago)
+#Númerp 22 
+Años=int(input('Ingresa cuantos años tienes : '))
+Segundos_de_vida= Años*31557600
+print('Has vivido por',Segundos_de_vida 'segundos')
+#Número 23 
+print(1,1,1,1,1)
+print(2,1,2,4,8)
+print(3,1,3,9,27)
+print(4,1,4,16,64)
+print(5,1,5,25,125)
